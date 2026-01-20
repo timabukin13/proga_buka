21cf1b7c673324cee1dee43a637cad46fec3100e
buka <161470535+timabukin13@users.noreply.github.com>
2026-01-20 14:07:55 +0300

    Update nums2.py
    очев

--- 9m/3_quarter/at_20.01/nums2.py
+++ 9m/3_quarter/at_20.01/nums2.py
@@ -1,1 +1,16 @@
+su = 0
+orger = True
+b = None
+a = 0
 
+for i in range(10):
+  a = int(input("Enter Number"))
+  su = su + a
+  if orger and b != None:
+    if a <= b:
+      orger = False
+  b = a
+
+if orger:
+  print("You wrote numbers in order")
+print("Sum is: " + str(su))

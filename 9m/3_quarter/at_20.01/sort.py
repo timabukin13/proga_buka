6a206929c8cbf74cf6b35427f38ca170ccd91073
buka <161470535+timabukin13@users.noreply.github.com>
2026-01-21 23:39:16 +0300

    Update sort.py
    написал код

--- 9m/3_quarter/at_20.01/sort.py
+++ 9m/3_quarter/at_20.01/sort.py
@@ -1,1 +1,19 @@
+def sort(arr):
+    n = len(arr)
+    if n <= 1:
+        return -1
+    for i in range(1, n):
+        now = arr[i]
+        j = i - 1
+        while j >= 0 and now < arr[j]:
+            arr[j + 1] = arr[j]
+            j -= 1
+            arr[j + 1] = now
+    return arr
 
+m = int(input("Введите число элементов массива "))
+arr = []
+
+for i in range(m):
+    arr.append(int(input()))
+print(sort(arr))
